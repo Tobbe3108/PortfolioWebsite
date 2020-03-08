ee0f29a69613e44384cf6c3677f49f7e023c3fe7
Tobbe3108 <Tobbe3108@gmail.com>
2020-03-08 23:42:04 +0100

    ready for main page

---
+++ PortfolioWebsite/Shared/PageState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioWebsite.Shared
+{
+    public class PageState
+    {
+        public bool FirstLoad { get; set; } = false;
+
+        public class Manager
+        {
+            private readonly PageState _State;
+
+            public Manager(PageState state)
+            {
+                _State = state;
+            }
+
+            public void SetFirstLoad(bool state)
+            {
+                _State.FirstLoad = state;
+                OnStateChanged?.Invoke();
+            }
+
+            public event Action OnStateChanged;
+        }
+    }
+}
